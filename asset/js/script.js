5ef8e91fadd94ebf502575376a0aebc3d023bf29
GildasTessier <140600513+GildasTessier@users.noreply.github.com>
2023-08-31 18:58:03 +0200

    add new navbar

---
+++ asset/js/script.js
@@ -0,0 +1,5 @@
+//For show items burger menu
+document.querySelector("#button-menu-burger").addEventListener("click", () => {
+    document.querySelector("#menu-mobile").classList.toggle("hidden");
+});
+
