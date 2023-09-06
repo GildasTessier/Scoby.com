39c68aa89f80a1a32bcb6a3edb6c4aa24c1ca8d6
GildasTessier <140600513+GildasTessier@users.noreply.github.com>
2023-09-06 18:01:44 +0200

    create function for btn Increase / Decrease in bloc products

--- asset/js/script.js
+++ asset/js/script.js
@@ -3,3 +3,10 @@ document.querySelector("#button-menu-burger").addEventListener("click", () => {
     document.querySelector("#menu-mobile").classList.toggle("hidden");
 });
 
+//For Increase/Decrease nb products to add
+let count = 0
+document.querySelector('#btnMoreLessProtucts').addEventListener('click', (event) => {
+    event.target.classList.contains('js-increase') ? count += 1 : count -= 1;
+    if (count < 0 ) count = 0;
+    document.querySelector('.js-nb-products').innerText = count;
+})
