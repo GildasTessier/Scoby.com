2417b89be8a0fe34b077f504651d38c474ff9462
GildasTessier <140600513+GildasTessier@users.noreply.github.com>
2023-09-06 19:03:03 +0200

    creat func for add products in page with template

--- asset/js/script.js
+++ asset/js/script.js
@@ -3,18 +3,38 @@ document.querySelector("#button-menu-burger").addEventListener("click", () => {
     document.querySelector("#menu-mobile").classList.toggle("hidden");
 });
 
-//For Increase/Decrease nb products to add
+
+async function waiting () {
+    try {
+        const response = await fetch("/datas/products.json");
+        return await response.json();
+    }
+    catch (e) {
+        console.error('Impossible de charger les données : ' + e);
+    }
+    
+}
+waiting()
+.then(displayProducts);
+
+function displayProducts(products) {
+    document.getElementById('ProductsClassic').append(...products.map(createProduct));
+}
+
+function createProduct(produit) {
+    const blocProduct = document.importNode(document.getElementById('productTemplate').content, true);
+    blocProduct.querySelector('.article-name').textContent = produit.name;
+    blocProduct.querySelector('.article-comp').textContent = produit.composition;
+    blocProduct.querySelector('.opt-size-1').textContent = produit.sizes[0] + ' cl';
+    blocProduct.querySelector('.opt-size-2').textContent = produit.sizes[1] + ' cl';
+    blocProduct.querySelector('.article-price').textContent = produit.prices[0] + '€';
+    
+    return blocProduct;
+}
+// For Increase/Decrease nb products to add
 // let count = 0
-// document.querySelector('#btnMoreLessProtucts').addEventListener('click', (event) => {
+// document.querySelector('.js-btnMoreLessProtucts').addEventListener('click', (event) => {
 //     event.target.classList.contains('js-increase') ? count += 1 : count -= 1;
 //     if (count < 0 ) count = 0;
 //     document.querySelector('.js-nb-products').innerText = count;
 // })
-
-async function waiting () {
-    const responce = await fetch("/datas/products.json");
-    const data = await responce.json();
-    console.log(data);
-
-}
-waiting();
