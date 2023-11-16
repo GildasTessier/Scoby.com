6b942dd2d3d7e9b3d4fb6a39d7196e53982b1e95
GildasTessier <140600513+GildasTessier@users.noreply.github.com>
2023-11-16 11:03:37 +0100

    add php in project

---
+++ _includes/_nav.php
@@ -0,0 +1,47 @@
+<nav class="nav-bar bg-green-dark">
+    <img id="icon-menu-mobile" class="icon-menu-mobile link-menu-icon" src="./asset/img/Icons/menu.png">
+    <div class="logo text-green-light">Scoby.com</div>
+
+    <ul class="menu-desktop" id="menu-desktop">
+        <li class="btn-menu-desktop"><a class="link-menu text-green-light" href="./index.html">Accueil</a></li>
+        <li class="btn-menu-desktop"><a class="link-menu text-green-light" href="./shop.html">Boutique</a></li>
+        <li class="btn-menu-desktop"><a class="link-menu text-green-light" href="./manufacturing.html">Fabrication</a></li>
+        <li class="btn-menu-desktop"><a class="link-menu text-green-light active-link-menu" href="./about.html">A popos /Nous contacter</a></li>
+        <li class="btn-menu-desktop"><a class="link-menu text-green-light" href="./account.html">Mon compte</a></li>
+    </ul>
+    <div class="icons-nav-bar">
+        <a class="icon-disconnect" href="#"><img class="img-icon-disconnect" src="asset/img/Icons/connect.png" alt="picto to connect"></a>
+        <div class="icon-order link-menu-icon" id="icon-order">
+            <img class="img-icon-order js-img-icon-order" src="./asset/img/Icons/drink-light.png" alt="pictogramme pannier">
+            <p class="count-order js-count-order bg-green-light text-green-dark ">0</p>
+        </div>
+    </div>
+    <ul class="menu-order bg-green-light bx-shadow hidden" id="menu-order">
+
+        <img id="close-order" class="img-close-order" src="./asset/img/Icons/close-page.png" alt="boutton fermer">
+
+        <li class="title-order">
+            <p class="title-order-name text-green-dark font-bold">Article</p>
+            <p class="title-order-qty text-green-dark font-bold">Nb</p>
+            <p class="title-order-total text-green-dark font-bold">Total</p>
+        </li>
+
+        <ul class="contained-order">
+        </ul>
+
+        <li class="total-order">
+            <p class="total-order-tx-total text-green-dark font-bold">Total commande :</p>
+            <p class="total-order-price js-total-order-price text-green-dark font-bold">0,00€</p>
+        </li>
+        <li><a class="btn-order bg-green-dark text-green-light font-bold bx-shadow btn-hover" href="./order.html">Commander</a></li>
+    </ul>
+
+    <ul class="menu-mobile bg-green-dark  bx-shadow hidden" id="menu-mobile">
+        <li class="btn-menu-mobile"><a class="link-menu text-green-light" href="./index.html">Accueil</a></li>
+        <li class="btn-menu-mobile"><a class="link-menu text-green-light" href="./shop.html">Boutique</a></li>
+        <li class="btn-menu-mobile"><a class="link-menu text-green-light" href="./manufacturing.html">Fabrication</a></li>
+        <li class="btn-menu-mobile active-link-menu"><a class="link-menu text-green-light active-link-menu" href="./about.html">A popos / Nous contacter</a></li>
+        <li class="btn-menu-mobile"><a class="link-menu text-green-light" href="./account.html">Mon compte</a></li>
+        <li class="btn-menu-mobile"><a class="link-menu text-green-light" href="#">Déconnexion</a></li>
+    </ul>
+</nav>
